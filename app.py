6f6a79c2eb965bc33ca9adf40f66e42d2bb022f8
Warayu Intawongs <109936823+intawongs@users.noreply.github.com>
2026-03-18 16:25:54 +0700

    test

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ elif menu == "🏆 Leaderboard Map":
                 # พิกัด x, y (อ้างอิงรูป 1024x1024)
                 POINTS = {
                     "Checkpoint 1": (1000,1500 ), 
-                    "Checkpoint 2": (820, 190),
+                    "Checkpoint 2": (720, 400),
                     "Start": (1250, 1750),
                     "Finish": (1250, 1750)
                 }
